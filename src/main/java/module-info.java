8ddd19b2e7d7ba557382c5a10a4f4c0508f06825
Dion Paul <dionpaul83@gmail.com>
2024-10-27 03:56:35 +0530

    remove unused dependancies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,6 @@ module assignment.thereadingroom {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
     requires java.sql;
 
     exports assignment.thereadingroom;
